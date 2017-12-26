05523d365e619aae569038c0014b19dafa3822d3
Ricky <rra.rickyresky@gmail.com>
2017-12-26 21:57:08 +0800

    aadsa
    asdasd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,27 +61,21 @@ class LoginController extends Controller
     /*BATAS MUSRENBANG*/
 
     // /*RKPD*/
-    // public function postLoginRKPD(Request $request){
-    //     // validasi data input
-    //     $this->validate($request,[
-    //         'username' => 'required|min:6|max:32',
-    //         'password' => 'required|min:6|max:32'
-    //     ]);
-    //     if (Auth::attempt(['username'=>$request->username,'password'=>$request->password,'program'=>'RKPD'],true)) {
-    //         Auth::login(Auth::user(), true);
-    //         return redirect('rkpd/administrator');
-    //     }else{
-    //         return redirect('rkpd')->with('aksesmasuk', 'Username atau Password Anda Salah !')->withInput($request->only('username', 'remember'));
-    //     }
-    // }
-    // /*BATAS RKPD*/
+    public function postLoginRKPD(Request $request){
+        // validasi data input
+        $this->validate($request,[
+            'username' => 'required|min:6|max:32',
+            'password' => 'required|min:6|max:32'
+        ]);
+        if (Auth::attempt(['username'=>$request->username,'password'=>$request->password,'program'=>'RKPD'],true)) {
+            Auth::login(Auth::user(), true);
+            return redirect('rkpd/administrator');
+        }else{
+            return redirect('rkpd')->with('aksesmasuk', 'Username atau Password Anda Salah !')->withInput($request->only('username', 'remember'));
+        }
+    }
+    /*BATAS RKPD*/
 
-    // public function logout(){
-    //     Auth::guard('web')->logout();
-    //     return redirect('/');
-
-    //     // return 'berhasil keluar';
-    // }
     public function logout($program){
         Auth::guard('web')->logout();
         if($program == 'rkpd')
---
+++ beranda/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<!--
+Template Name: Pesaton
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>e-DevPlan Kab. Yalimo</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="beranda/layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('beranda/images/bg.jpg');"> 
+  <!-- ################################################################################################ -->
+<!--  <div class="wrapper row1">-->
+<!--    <header id="header" class="hoc clear"> -->
+      <!-- ################################################################################################ -->
+<!--      <div id="logo" class="fl_left" >-->
+          <!-- <a href="#" style="font-size:30px">e-Planning Kab. Yalimo</a><br><br> -->
+<!--      </div>-->
+      <!-- ################################################################################################ -->
+<!--    </header>-->
+<!--  </div>-->
+    
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article class="clear">
+      <ul class="nospace clear">
+        <li style="font-size:12px;"><a href="rpjmd/index.php" target="_blank"><i class="fa fa-list-alt"></i>e-RPJMD</a></li>
+        <li style="font-size:12px;"><a href="musrenbang"><i class="fa fa-group"></i>e-Musrenbang</a></li>
+        <li style="font-size:12px;"><a href="rkpd/index.php"><i class="fa fa-file-text-o"></i>e-RKPD</a></li>
+      </ul>
+      <p id="example1" style="font-size:20px"></p>
+      <footer><a class="btn" href="#penjelasan">Deskripsi</a></footer>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3" id="penjelasan">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="group cta">
+      <article class="one_third first"><a href="#"><i class="fa fa-list-alt"></i></a>
+        <h6 class="heading">e-RPJMD</h6>
+        <p>e-RPJMD merupakan Sistem Informasi Manajemen Perencanaan yang mencakup Visi Misi kepala Daerah sampai kepada penerapannya ke dalam Rencana Strategis Perangkat Daerah (RENSTRA) selama periode 5 Tahun Visi,Misi,Tujuan,Sasaran & Indikator Sasaran,Strategi,Kebijakan, Program Kegiatan beserta pagu Indikatif Permendagri 54/2010 Bab V Pasal 50</p>
+      </article>
+      <article class="one_third"><a href="#"><i class="fa fa-group"></i></a>
+        <h6 class="heading">e-MUSRENBANG</h6>
+        <p>e-Musrenbang merupakan Sistem Informasi Manajemen Perencanaan Pembangunan yang digunakan untuk memfasilitasi Kab/Kota dalam mengusulkan Program/Kegiatan Prioritas Pembangunan Daerah yang menghasilkan berita acara kesepakatan hasil musrenbang dan Laporan Usulan Kegiatan Pokok-Pokok Pikiran DPRD Permendagri 54/2010 Pasal 118</p>
+      </article>
+      <article class="one_third"><a href="#"><i class="fa fa-file-text-o"></i></a>
+        <h6 class="heading">e-RKPD</h6>
+        <p>e-RKPD merupakan Sistem Informasi Manajemen Perencanaan yang memuat Rancangan Ekonomi Daerah, Program Prioritas Pembangunan Daerah, Rencana Kerja Pendanaan dan Prakiraan Maju yang menghasilkan dokumen RKPD dan RENJA PD Permendagri 54/2010 Bab VII Pasal 99</p>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+	<div>
+	
+		<h1></h1>
+		<center><img src="beranda/images/petawilayah.jpg" width="150px" height="150px"></center>
+		
+	</div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2017 - Powered By Bappeda Yalimo</p>
+    <!--<p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="beranda/layout/scripts/jquery.min.js"></script>
+<script src="beranda/layout/scripts/jquery.backtotop.js"></script>
+<script src="beranda/layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="beranda/layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+<script src="beranda/layout/scripts/jquery-3.0.0.min.js"></script> 
+<script src="beranda/layout/scripts/typeit.min.js"></script>
+<script>
+    $('#example1').typeIt({
+         strings: ["Rencana Pembangunan Jangka Menengah Daerah (e-RPJMD)", "Musyawarah Perencanaan Pembangunan (e-Musrenbang)", "Rencana Kerja Pemerintah Daerah (e-RKPD)"],
+         speed: 100,
+         breakLines: false,
+         loop : true,
+         loopDelay : 750,
+         autoStart: false
+    });
+</script>
+</body>
+</html>
