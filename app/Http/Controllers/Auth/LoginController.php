6c6300a9d1136a88eafb8f333c313bf554268303
Ricky <rra.rickyresky@gmail.com>
2017-12-26 20:39:51 +0800

    asdasd
    asdasd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,11 +39,6 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function logout($program){
-        Auth::guard('web')->logout();
-        return redirect($program);
-    }
-
     /*MUSRENBANG*/
     public function postLogin(Request $request){
         // validasi data input
@@ -65,19 +60,35 @@ class LoginController extends Controller
     }
     /*BATAS MUSRENBANG*/
 
-    /*RKPD*/
-    public function postLoginRKPD(Request $request){
-        // validasi data input
-        $this->validate($request,[
-            'username' => 'required|min:6|max:32',
-            'password' => 'required|min:6|max:32'
-        ]);
-        if (Auth::attempt(['username'=>$request->username,'password'=>$request->password,'program'=>'RKPD'],true)) {
-            Auth::login(Auth::user(), true);
-            return redirect('rkpd/administrator');
-        }else{
-            return redirect('rkpd')->with('aksesmasuk', 'Username atau Password Anda Salah !')->withInput($request->only('username', 'remember'));
-        }
+    // /*RKPD*/
+    // public function postLoginRKPD(Request $request){
+    //     // validasi data input
+    //     $this->validate($request,[
+    //         'username' => 'required|min:6|max:32',
+    //         'password' => 'required|min:6|max:32'
+    //     ]);
+    //     if (Auth::attempt(['username'=>$request->username,'password'=>$request->password,'program'=>'RKPD'],true)) {
+    //         Auth::login(Auth::user(), true);
+    //         return redirect('rkpd/administrator');
+    //     }else{
+    //         return redirect('rkpd')->with('aksesmasuk', 'Username atau Password Anda Salah !')->withInput($request->only('username', 'remember'));
+    //     }
+    // }
+    // /*BATAS RKPD*/
+
+    // public function logout(){
+    //     Auth::guard('web')->logout();
+    //     return redirect('/');
+
+    //     // return 'berhasil keluar';
+    // }
+    public function logout($program){
+        Auth::guard('web')->logout();
+        if($program == 'rkpd')
+            return redirect($program);
+        else
+            return redirect('/');
+
+        // return 'berhasil keluar';
     }
-    /*BATAS RKPD*/
 }
